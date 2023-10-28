28d1bbc6001c6ec12c0b2da99b4525fbc72a1d0c
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-10-28 02:44:25 -0300

    Refatora e adiciona mapas disponiveis

--- events/logMap.js
+++ events/logMap.js
@@ -4,100 +4,68 @@ const cron = require('cron')
 require("dotenv").config()
 const mongoose = require("mongoose")
 const LogMaps = require("../schemes/logMaps")
+
 let messageId = null
 
 client.once("ready", () => {
     const attMapsLog = new cron.CronJob("*/1 * * * *", async () => {
+        const guildID = '1110388609074344017'
+        const canal = '1160848506102235197'
 
-        let guildID = '1110388609074344017'
-        let canal = '1160848506102235197'
         const guild = client.guilds.cache.get(guildID)
         const channel = guild.channels.cache.get(canal)
 
-        mongoose.connect(process.env.MONGO, {
+        await mongoose.connect(process.env.MONGO, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
 
         const resultados = await LogMaps.find()
 
-        const hoje = new Date();
-       // hoje.setDate(hoje.getDate() - 1)
+        const hoje = new Date()
 
-        const mapasRodadosHoje = []
-        resultados.forEach(resultado => {
-            resultado.mapas.forEach(mapa => {
-                const ultimaVezRodado = new Date(mapa.ultimaVezRodado);
+        const tresDiasAtras = new Date()
+        tresDiasAtras.setDate(tresDiasAtras.getDate() - 3)
 
-                if (ultimaVezRodado.getDate() === hoje.getDate() &&
-                    ultimaVezRodado.getMonth() === hoje.getMonth() &&
-                    ultimaVezRodado.getFullYear() === hoje.getFullYear()) {
-                    mapasRodadosHoje.push({
-                        nome: mapa.nome,
-                        vezesRodado: mapa.vezesRodado
-                    })
-                    mapasRodadosHoje.sort((a, b) => {
-                        return a.nome.localeCompare(b.nome);
-                    });
-                }
-            })
-        })
+        const mapasRodadosUltimos3Dias = resultados.flatMap(resultado =>
+            resultado.mapas.filter(mapa =>
+                new Date(mapa.ultimaVezRodado) >= tresDiasAtras && new Date(mapa.ultimaVezRodado) <= hoje
+            ).map(mapa => ({ nome: mapa.nome, vezesRodado: mapa.vezesRodado, ultimaVezRodado: new Date(mapa.ultimaVezRodado) }))
+        ).sort((a, b) => b.ultimaVezRodado - a.ultimaVezRodado)
 
-        let mapasString = ''
+        const mapasString3 = mapasRodadosUltimos3Dias.map(mapa => mapa.nome).join('\n')
 
-        mapasRodadosHoje.forEach(mapa => {
-            mapasString += `${mapa.nome}\n`
-        })
+        // const mapasNaoRodadosUltimos3Dias = resultados.flatMap(resultado =>
+        //     resultado.mapas.filter(mapa =>
+        //         new Date(mapa.ultimaVezRodado) < tresDiasAtras
+        //     ).map(mapa => mapa.nome)
+        // ).sort((a, b) => a.localeCompare(b))
 
-        const tresDiasAtras = new Date();
-        tresDiasAtras.setDate(tresDiasAtras.getDate() - 3);
-        const mapasRodadosUltimos3Dias = [];
+        // const mapasStringNaoRodados = mapasNaoRodadosUltimos3Dias.join('\n')
 
-        resultados.forEach(resultado => {
-            resultado.mapas.forEach(mapa => {
-                const ultimaVezRodado = new Date(mapa.ultimaVezRodado);
+        const mapasNaoRodadosUltimos3DiasOrdenados = resultados.flatMap(resultado =>
+            resultado.mapas.filter(mapa =>
+                new Date(mapa.ultimaVezRodado) < tresDiasAtras
+            ).map(mapa => ({ nome: mapa.nome, ultimaVezRodado: mapa.ultimaVezRodado }))
+        ).sort((a, b) => b.ultimaVezRodado - a.ultimaVezRodado)
+        
+        const mapasStringNaoRodadosOrdenados = mapasNaoRodadosUltimos3DiasOrdenados.map(mapa => mapa.nome).join('\n')
 
-                if (ultimaVezRodado >= tresDiasAtras && ultimaVezRodado <= hoje) {
-                    mapasRodadosUltimos3Dias.push({
-                        nome: mapa.nome,
-                        vezesRodado: mapa.vezesRodado
-                    });
-                    mapasRodadosUltimos3Dias.sort((a, b) => {
-                        return a.nome.localeCompare(b.nome);
-                    });
-                }
-            });
-        });
-
-        let mapasString3 = '';
-
-        mapasRodadosUltimos3Dias.forEach(mapa => {
-            mapasString3 += `${mapa.nome}\n`;
-        });
-
-        let embed = new Discord.EmbedBuilder()
+        const embed = new Discord.EmbedBuilder()
             .setColor("#034733")
-            .setTitle("Log Maps Reality Brasil")
+            .setTitle("🗺️ Log Maps Reality Brasil 🗺️")
             .addFields(
-                {
-                    name: `3 DIAS`,
-                    value: '```' + `${mapasString3}` + '```',
-                    inline: true
-                },
-                {
-                    name: `HOJE`,
-                    value: '```' + `${mapasString}` + '```',
-                    inline: true
-                }
+                // { name: "DISPONÍVEL", value: '```' + `${mapasStringNaoRodados}` + '```', inline: true },
+                { name: "DISPONIVEL 🟩", value: '```' + `${mapasStringNaoRodadosOrdenados}` + '```', inline: true },
+                { name: "RODADOS 🟧", value: '```' + `${mapasString3}` + '```', inline: true },
             )
+            .setFooter({ text: 'Mapas ordenados por data decrescente da última vez rodado'})
             .setTimestamp()
-        if (messageId == null) {
-            const message = await channel.send({ embeds: [embed] })
-            messageId = message.id
-        } else if (messageId != null) {
-            const message = await channel.messages.fetch(messageId)
-            message.edit({ embeds: [embed] })
-        }
+
+        const message = messageId ? await channel.messages.fetch(messageId) : await channel.send({ embeds: [embed] })
+        message.edit({ embeds: [embed] })
+        messageId = message.id
     })
+
     attMapsLog.start()
 })
