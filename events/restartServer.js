59c5d132922fb3ffaaf63a51889e2951adc6559a
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-11-11 01:22:04 -0300

    corrige dia

--- events/restartServer.js
+++ events/restartServer.js
@@ -9,7 +9,7 @@ client.once("ready", () => {
     const guild = client.guilds.cache.get(guildID)
     const channel = guild.channels.cache.get(canal)
 
-    const restartServer10 = new cron.CronJob("1 7 * * *", async () => {
+    const restartServer10 = new cron.CronJob("1 7 * * 1,4", async () => {
         await Promise.all([
             channel.send({ content: '!say Server será reinciciado em 10 minutos!'}),
         ])
