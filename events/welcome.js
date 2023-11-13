40b505951729ececd72db3f2fce40d43ac94cba7
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-11-13 00:58:09 -0300

    Altera estrutura do evento

--- events/welcome.js
+++ events/welcome.js
@@ -1,15 +1,43 @@
 const client = require("..")
 const Discord = require('discord.js')
+const { AttachmentBuilder } = require('discord.js')
 
-client.on('guildMemberAdd', (member) => {
-    let canal = '1149614821411655700'
-    if (!canal) return
+client.on('guildMemberAdd', async (member) => {
+    const canalID = '1149614821411655700'
+    if (!canalID) return
 
-    let embed = new Discord.EmbedBuilder()
-    .setColor("#034733")
-    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-    .setTitle("Bem-vindo(a)!")
-    .setDescription(`Olá, ${member}. Obrigado(a) por entrar no Discord da Reality Brasil! Não se esqueça de acessar as regras de nosso servidor: https://docs.realitybrasil.games/`)
+    const Canvas = require('canvas')
+    const canvas = Canvas.createCanvas(832, 245)
+    const ctx = canvas.getContext('2d')
 
-    member.guild.channels.cache.get(canal).send({embeds: [embed], content: `${member}`})
+    const avatarSize = 116
+    const avatarRadius = avatarSize / 2
+    const avatarX = canvas.width / 2 - avatarRadius - 280
+    const avatarY = canvas.height / 2 - avatarRadius
+
+    const background = await Canvas.loadImage('https://i.imgur.com/fDqbcZ5.png')
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
+
+    function drawCircularAvatar() {
+        ctx.beginPath()
+        ctx.arc(avatarX + avatarRadius, avatarY + avatarRadius, avatarRadius, 0, Math.PI * 2, true)
+        ctx.closePath()
+        ctx.clip()
+        ctx.drawImage(avatarImg, avatarX, avatarY, avatarSize, avatarSize)
+    }
+
+    const avatar = member.user.displayAvatarURL({ extension: 'png'})
+    const avatarImg = await Canvas.loadImage(avatar)
+
+    drawCircularAvatar()
+
+    const attachment = new AttachmentBuilder(canvas.toBuffer(), { name: 'welcome-image.png' })
+
+    const canal = member.guild.channels.cache.get(canalID)
+    if (canal) {
+        canal.send({
+            content: `Olá, ${member} obrigado(a) por entrar no Discord da Reality Brasil! Fique atento(a) às <#1149599531185487893> e fique ligado(a) no <#1149603095823790111>`,
+            files: [attachment],
+        })
+    }
 })
