cd95d2b0b4ea722e53adc865d617a12b81144ef7
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-12-18 06:58:58 -0300

    corrige loop

--- events/previousMap.js
+++ events/previousMap.js
@@ -69,7 +69,7 @@ client.once("ready", () => {
                 cor = '#00A254'
             } else if (playersP >= 46 && playersP <= 50) {
                 cor = '#00A21A'
-            }else if (playersP >= 51 && playersP <= 55) {
+            } else if (playersP >= 51 && playersP <= 55) {
                 cor = '#0ABB06'
             } else if (playersP >= 56 && playersP <= 60) {
                 cor = '#35BB06'
@@ -90,18 +90,17 @@ client.once("ready", () => {
             } else if (playersP >= 96 && playersP <= 100) {
                 cor = '#FF0000'
             }
-             else {
+            else {
                 cor = '#ffffff'
             }
-
-            let team1PlayersScoreBoard = (team1Players && team1Players.length > 0) ? team1Players.map(player => player.name + ' ' + player.kills + '|' + player.deaths + ' ' + player.score).join('\n') : '-'
-            let team2PlayersScoreBoard = (team2Players && team2Players.length > 0) ? team2Players.map(player => player.name + ' ' + player.kills + '|' + player.deaths + ' ' + player.score).join('\n') : '-'
 
             let totalPlayersTeam1 = 0
             let totalKillsTeam1 = 0
             let totalDeathsTeam1 = 0
             let totalScoreTeam1 = 0
 
+
+            if (Array.isArray(team1Players) && team1Players.length > 0) {
             for (const player of team1Players) {
                 totalPlayersTeam1++
                 totalKillsTeam1 += player.kills
@@ -118,95 +117,100 @@ client.once("ready", () => {
                     lastTotalScoreTeam1 = totalScoreTeam1
                 }
             }
+        }
 
             let totalPlayersTeam2 = 0
             let totalKillsTeam2 = 0
             let totalDeathsTeam2 = 0
             let totalScoreTeam2 = 0
 
-            for (const player of team2Players) {
-                totalPlayersTeam2++
-                totalKillsTeam2 += player.kills
-                totalDeathsTeam2 += player.deaths
-                totalScoreTeam2 += player.score
 
-                if (player.deaths !== 0) {
-                    lastTotalDeathsTeam2 = totalDeathsTeam2
-                }
-                if (player.kills !== 0) {
-                    lastTotalKillsTeam2 = totalKillsTeam2
-                }
-                if (player.score !== 0) {
-                    lastTotalScoreTeam2 = totalScoreTeam2
-                }
+            if (Array.isArray(team2Players) && team2Players.length > 0) {
 
+                for (const player of team2Players) {
+                    totalPlayersTeam2++
+                    totalKillsTeam2 += player.kills
+                    totalDeathsTeam2 += player.deaths
+                    totalScoreTeam2 += player.score
+
+                    if (player.deaths !== 0) {
+                        lastTotalDeathsTeam2 = totalDeathsTeam2
+                    }
+                    if (player.kills !== 0) {
+                        lastTotalKillsTeam2 = totalKillsTeam2
+                    }
+                    if (player.score !== 0) {
+                        lastTotalScoreTeam2 = totalScoreTeam2
+                    }
+
+                }
             }
 
-            let embed = new Discord.EmbedBuilder()
-                .setColor(cor)
-                .setAuthor({ name: `=RB= REALITY BRASIL MAPS`, iconURL: 'https://i.imgur.com/wlYkc17.png', url: 'https://realitybrasil.games/battlerecorder' })
-                .setTitle(`🗺️〡${mapName}`)
-                .addFields(
-                    {
-                        name: `${serverInfo.gameTypeEx}・${serverInfo.gameLayoutEx}`,
-                        value: `👥 ${playersP}/${playersT}`,
-                        inline: false
-                    },
-                    {
-                        name: `🔵 ${team2}`,
-                        value: '```' + `Players: ${totalPlayersTeam2}\nScore: ${lastTotalScoreTeam2}\nKills: ${lastTotalKillsTeam2} \nDeaths: ${lastTotalDeathsTeam2}` + '```',
-                        inline: true
-                    },
-                    {
-                        name: `🔴 ${team1}`,
-                        value: '```' + `Players: ${totalPlayersTeam1}\nScore: ${lastTotalScoreTeam1}\nKills: ${lastTotalKillsTeam1} \nDeaths: ${lastTotalDeathsTeam1}` + '```',
-                        inline: true
-                    }
-                )
-                .setImage(image_link)
-                .setFooter({ text: 'Atualizado', iconURL: 'https://i.imgur.com/iBCQ8MS.png' })
-                .setTimestamp()
-
-            if (messageId && previousMap == mapName) {
-                const message = await channel.messages.fetch(messageId)
-                message.edit({ embeds: [embed] })
-            } else if (messageId != null || previousMap != mapName) {
-                const message = await channel.send({ embeds: [embed] })
-                messageId = message.id
-                previousMap = mapName
-                const existingMap = await logMaps.findOne({ 'mapas.nome': mapName });
-                if (serverInfo.gameTypeEx == "AAS" || serverInfo.gameTypeEx == "Insurgency") {
-                    if (existingMap) {
-                        await logMaps.updateOne(
-                            { 'mapas.nome': mapName },
-                            {
-                                $inc: { 'mapas.$.vezesRodado': 1 },
-                                $set: { 'mapas.$.ultimaVezRodado': new Date() }
-                            }
-                        )
-                    } else {
-                        await logMaps.findOneAndUpdate({ _id: 'mapasRodados' }, {
-                            $addToSet: {
-                                mapas: [{
-                                    nome: mapName,
-                                    vezesRodado: 1,
-                                    ultimaVezRodado: new Date()
-                                }]
-                            }
+                let embed = new Discord.EmbedBuilder()
+                    .setColor(cor)
+                    .setAuthor({ name: `=RB= REALITY BRASIL MAPS`, iconURL: 'https://i.imgur.com/wlYkc17.png', url: 'https://realitybrasil.games/battlerecorder' })
+                    .setTitle(`🗺️〡${mapName}`)
+                    .addFields(
+                        {
+                            name: `${serverInfo.gameTypeEx}・${serverInfo.gameLayoutEx}`,
+                            value: `👥 ${playersP}/${playersT}`,
+                            inline: false
                         },
-                            { upsert: true })
+                        {
+                            name: `🔵 ${team2}`,
+                            value: '```' + `Players: ${totalPlayersTeam2}\nScore: ${lastTotalScoreTeam2}\nKills: ${lastTotalKillsTeam2} \nDeaths: ${lastTotalDeathsTeam2}` + '```',
+                            inline: true
+                        },
+                        {
+                            name: `🔴 ${team1}`,
+                            value: '```' + `Players: ${totalPlayersTeam1}\nScore: ${lastTotalScoreTeam1}\nKills: ${lastTotalKillsTeam1} \nDeaths: ${lastTotalDeathsTeam1}` + '```',
+                            inline: true
+                        }
+                    )
+                    .setImage(image_link)
+                    .setFooter({ text: 'Atualizado', iconURL: 'https://i.imgur.com/iBCQ8MS.png' })
+                    .setTimestamp()
+
+                if (messageId && previousMap == mapName) {
+                    const message = await channel.messages.fetch(messageId)
+                    message.edit({ embeds: [embed] })
+                } else if (messageId != null || previousMap != mapName) {
+                    const message = await channel.send({ embeds: [embed] })
+                    messageId = message.id
+                    previousMap = mapName
+                    const existingMap = await logMaps.findOne({ 'mapas.nome': mapName });
+                    if (serverInfo.gameTypeEx == "AAS" || serverInfo.gameTypeEx == "Insurgency") {
+                        if (existingMap) {
+                            await logMaps.updateOne(
+                                { 'mapas.nome': mapName },
+                                {
+                                    $inc: { 'mapas.$.vezesRodado': 1 },
+                                    $set: { 'mapas.$.ultimaVezRodado': new Date() }
+                                }
+                            )
+                        } else {
+                            await logMaps.findOneAndUpdate({ _id: 'mapasRodados' }, {
+                                $addToSet: {
+                                    mapas: [{
+                                        nome: mapName,
+                                        vezesRodado: 1,
+                                        ultimaVezRodado: new Date()
+                                    }]
+                                }
+                            },
+                                { upsert: true })
+                        }
+
                     }
 
+                    lastTotalKillsTeam1 = 0
+                    lastTotalDeathsTeam1 = 0
+                    lastTotalScoreTeam1 = 0
+                    lastTotalKillsTeam2 = 0
+                    lastTotalDeathsTeam2 = 0
+                    lastTotalScoreTeam2 = 0
                 }
-
-                lastTotalKillsTeam1 = 0
-                lastTotalDeathsTeam1 = 0
-                lastTotalScoreTeam1 = 0
-                lastTotalKillsTeam2 = 0
-                lastTotalDeathsTeam2 = 0
-                lastTotalScoreTeam2 = 0
             }
-        }
-    })
+        })
     attMap.start()
 })
